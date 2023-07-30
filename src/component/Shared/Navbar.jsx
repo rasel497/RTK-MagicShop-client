d1fa80a41b9559b8b1bd4ffee750fe3825cf7ddd
rasel497 <rasel.csediu44@gmail.com>
2023-07-30 23:55:00 +0600

    added mobile responsive navbar

--- src/component/Shared/Navbar.jsx
+++ src/component/Shared/Navbar.jsx
@@ -51,23 +51,41 @@ const Navbar = () => {
         </React.Fragment>
     return (
         <section>
-            <div className="navbar bg-base-100 flex justify-between" style={{ backgroundColor: "#0088CC" }}>
-                <div className="navbar-start">
-                    <div className="dropdown">
-                        <label tabIndex={0} className="btn btn-ghost lg:hidden">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-                        </label>
-                        <ul tabIndex={1} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+            <div className="navbar bg-base-100" style={{ backgroundColor: "#0088CC" }}>
+                <div className="flex-1">
+                    <Link to='/' className="btn btn-ghost normal-case text-2xl font-bold"><span className='text-white'>MagicShop</span></Link>
+                </div>
+                <div className="flex-none gap-2">
+                    <div>
+                        <SearchFilter />
+                    </div>
+                    <div className="dropdown dropdown-end">
+                        <div className="dropdown">
+                            <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                            </label>
+                        </div>
+                        <ul className="menu menu-horizontal p-0 text-white">
                             {menuItems}
                         </ul>
                     </div>
-                    <Link to='/' className="btn btn-ghost normal-case text-2xl font-bold"><span className='text-white'>MagicShop</span></Link>
-                </div>
-                <SearchFilter />
-                <div className="navbar-center hidden lg:flex">
-                    <ul className="menu menu-horizontal p-0 text-white">
-                        {menuItems}
-                    </ul>
+
+                    {/* <div className="dropdown dropdown-end">
+                        <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+                            <div className="w-10 rounded-full">
+                                <img src="https://i.ibb.co/YhK840h/myPhoto1.png" alt='' />
+                            </div>
+                        </label>
+                        <ul tabIndex={0} className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
+                            <li>
+                                <Link to="/profile" className="justify-between">
+                                    Profile
+                                    <span className="badge">New</span>
+                                </Link>
+                            </li>
+                            {menuItems}
+                        </ul>
+                    </div> */}
                 </div>
             </div>
         </section>
--- src/component/Shared/SearchFilter.jsx
+++ src/component/Shared/SearchFilter.jsx
@@ -1,26 +1,25 @@
 import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
+import './search.css';
 
 const SearchFilter = () => {
     const { products } = useSelector((state) => state.products);
     const [filter, setFilter] = useState("");
 
     return (
-        <div>
-            <div className="form-control">
-                <label className="input-group input-group-sm">
-                    <input
-                        type="text"
-                        placeholder="search product"
-                        className="input input-bordered input-sm"
-                        onChange={(event) => { setFilter(event.target.value) }}
-                    />
-                    <span><svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg></span>
-                </label>
+        <>
+            <div class="search ">
+                <span class="fa fa-search"></span>
+                <input
+                    type="text"
+                    placeholder="search product"
+                    className="input input-bordered input-sm "
+                    onChange={(event) => { setFilter(event.target.value) }}
+                />
             </div>
             <div>
                 {
-                    products.filter((searchValue) => {
+                    Array.isArray(products) && products.filter((searchValue) => {
                         if (setFilter === "") {
                             return searchValue;
                         }
@@ -51,7 +50,7 @@ const SearchFilter = () => {
                         })
                 }
             </div>
-        </div>
+        </>
     );
 };
 
